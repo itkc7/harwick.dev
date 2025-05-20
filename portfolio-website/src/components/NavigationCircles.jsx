aedb23288f51f6723a9976fc351dff22e3a9d755
itkc7 <kyle.harwick29@gmail.com>
2025-05-20 01:28:22 -0400

    navigation circle scrolling logic

--- portfolio-website/src/components/NavigationCircles.jsx
+++ portfolio-website/src/components/NavigationCircles.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from 'react';
 
 const NavigationCircles = ({section}) => {
   return (
@@ -6,30 +6,38 @@ const NavigationCircles = ({section}) => {
       className="h-[300px] w-[1px] bg-red-500 dark:bg-red-500 absolute right-12 hidden md:flex 
     flex-col justify-between items-center transition-colors duration-500"
     >
-      <div
-        className={`w-5 aspect-square border border-red-500  dark:border-red-500 rounded-full
+      <a href="#home">
+        <div
+          className={`w-5 aspect-square border border-red-500  dark:border-red-500 rounded-full
       bg-gray-300 transition-colors duration-500 ${
         section === "home" ? "bg-red-500 dark:bg-red-500" : "bg-gray-300"
       }`}
-      ></div>
-      <div
-        className={`w-5 aspect-square border border-red-500  dark:border-red-500 rounded-full
+        ></div>
+      </a>
+      <a href="#services">
+        <div
+          className={`w-5 aspect-square border border-red-500  dark:border-red-500 rounded-full
       bg-gray-300 transition-colors duration-500 ${
         section === "services" ? "bg-red-500 dark:bg-red-500" : "bg-gray-300"
       }`}
-      ></div>
-      <div
-        className={`w-5 aspect-square border border-red-500  dark:border-red-500 rounded-full
+        ></div>
+      </a>
+      <a href="#lastfm">
+        <div
+          className={`w-5 aspect-square border border-red-500  dark:border-red-500 rounded-full
       bg-gray-300 transition-colors duration-500 ${
         section === "lastfm" ? "bg-red-500 dark:bg-red-500" : "bg-gray-300"
       }`}
-      ></div>
-      <div
-        className={`w-5 aspect-square border border-red-500  dark:border-red-500 rounded-full
+        ></div>
+      </a>
+      <a href="#contact">
+        <div
+          className={`w-5 aspect-square border border-red-500  dark:border-red-500 rounded-full
       bg-gray-300 transition-colors duration-500 ${
         section === "contact" ? "bg-red-500 dark:bg-red-500" : "bg-gray-300"
       }`}
-      ></div>
+        ></div>
+      </a>
     </div>
   );
     
