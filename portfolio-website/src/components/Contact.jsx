34621791bb98e9a2b0b4295ae3cae3a9a67f0632
kyle <kyle.harwick29@gmail.com>
2025-05-19 21:49:30 -0400

    Update Contact.jsx

--- portfolio-website/src/components/Contact.jsx
+++ portfolio-website/src/components/Contact.jsx
@@ -16,29 +16,55 @@ const Contact = () => {
         data-netlify="true"
         className="flex flex-col lg:space-y-12 space-y-8"
       >
-        <input
-          type="email"
-          name="email"
-          placeholder="Email"
-          className="md:w-[500px] w-[330px] h-13
-              pl-3 text-lg outline-0 border border-red-500 dark:border-red-500 placeholder:gray-600 dark:placeholder:red-500/50
-              transition-colors duration-500"
-          required
-        />
-        <textarea
-          name="message"
-          placeholder="Message"
-          className="md:w-[500px] w-[330px] h-13 pl-3 text-lg outline-0 border border-red-500 dark:border-red-500 placeholder:gray-600 dark:placeholder:red-500/50
-            min-h-[100px] max-h-[1200px] resize-y p-3 transition-colors duration-500"
-          required
-        ></textarea>
-        <input
-          type="submit"
-          value="Stay Connected"
-          className="md:w-[500px] w-[330px] h-13
-              pl-3 text-lg outline-0 bg-red-500 dark:bg-red-500 text-white dark:text-gray-900 uppercase font-extrabold
-              cursor-pointer tracking-wide shadow-md shadow-gray-700/20 transition-colors duration-500"
-        />
+        <input type="hidden" name="form-name" value="contact" />
+
+        <div>
+          <label className="sr-only">Your Name</label>
+          <input
+            type="text"
+            name="name"
+            placeholder="Your Name"
+            className="md:w-[500px] w-[330px] h-13
+                pl-3 text-lg outline-0 border border-red-500 dark:border-red-500 placeholder:gray-600 dark:placeholder:red-500/50
+                transition-colors duration-500"
+            required
+          />
+        </div>
+
+        <div>
+          <label className="sr-only">Your Email</label>
+          <input
+            type="email"
+            name="email"
+            placeholder="Your Email"
+            className="md:w-[500px] w-[330px] h-13
+                pl-3 text-lg outline-0 border border-red-500 dark:border-red-500 placeholder:gray-600 dark:placeholder:red-500/50
+                transition-colors duration-500"
+            required
+          />
+        </div>
+
+        <div>
+          <label className="sr-only">Message</label>
+          <textarea
+            name="message"
+            placeholder="Your Message"
+            className="md:w-[500px] w-[330px] h-13 pl-3 text-lg outline-0 border border-red-500 dark:border-red-500 placeholder:gray-600 dark:placeholder:red-500/50
+              min-h-[100px] max-h-[1200px] resize-y p-3 transition-colors duration-500"
+            required
+          ></textarea>
+        </div>
+
+        <div>
+          <button
+            type="submit"
+            className="md:w-[500px] w-[330px] h-13
+                pl-3 text-lg outline-0 bg-red-500 dark:bg-red-500 text-white dark:text-gray-900 uppercase font-extrabold
+                cursor-pointer tracking-wide shadow-md shadow-gray-700/20 transition-colors duration-500"
+          >
+            Stay Connected
+          </button>
+        </div>
       </form>
       <NavigationCircles section="contact" />
     </div>
