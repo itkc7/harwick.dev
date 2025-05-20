5cb7dfe78ec3ad3aa63d376a50eb7886697da0a6
kyle <kyle.harwick29@gmail.com>
2025-05-19 21:37:15 -0400

    Update Contact.jsx

--- portfolio-website/src/components/Contact.jsx
+++ portfolio-website/src/components/Contact.jsx
@@ -3,47 +3,32 @@ import NavigationCircles from "./NavigationCircles";
 
 const Contact = () => {
   return (
-    <div
-      id="contact"
-      className="h-screen flex flex-col justify-center items-center"
-    >
-      <h2 className="text-4xl font-light md:mb-32 mb-24 dark:text-white">
-        Connect with me!
-      </h2>
-      <form
-        name="contact"
-        method="POST"
-        data-netlify="true"
-        className="flex flex-col lg:space-y-12 space-y-8"
-      >
-        {/* Add a hidden input with the form name for Netlify */}
-        <input type="hidden" name="form-name" value="contact" />
-
-        <input
+    <div id="contact" className="h-screen flex flex-col justify-center items-center">
+      <h2 className="text-4xl font-light md:mb-32 mb-24 dark:text-white"> Connect with me!</h2>
+      <form className="flex flex-col lg:space-y-12 space-y-8">
+        <form
           type="email"
-          name="email"
           placeholder="Email"
+          netlify
           className="md:w-[500px] w-[330px] h-13
               pl-3 text-lg outline-0 border border-red-500 dark:border-red-500 placeholder:gray-600 dark:placeholder:red-500/50
               transition-colors duration-500"
-          required
         />
         <textarea
-          name="message"
           placeholder="Message"
           className="md:w-[500px] w-[330px] h-13 pl-3 text-lg outline-0 border border-red-500 dark:border-red-500 placeholder:gray-600 dark:placeholder:red-500/50
             min-h-[100px] max-h-[1200px] resize-y p-3 transition-colors duration-500"
-          required
         ></textarea>
-        <input
+        <form
           type="submit"
           value="Stay Connected"
+          netlify
           className="md:w-[500px] w-[330px] h-13
               pl-3 text-lg outline-0 bg-red-500  dark:bg-red-500 text-white dark:text-gray-900 uppercase font-extrabold
               cursor-pointer tracking-wide shadow-md shadow-gray-700/20 transition-colors duration-500"
         />
-      </form>
-      <NavigationCircles section="contact" />
+          </form>
+          <NavigationCircles section="contact" />
     </div>
   );
 };
