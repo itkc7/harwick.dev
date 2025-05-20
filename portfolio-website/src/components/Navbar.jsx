00fd1932065de114b0040c7808dcc03a510c219b
itkc7 <kyle.harwick29@gmail.com>
2025-05-20 17:49:31 -0400

    even more adjusting

--- portfolio-website/src/components/Navbar.jsx
+++ portfolio-website/src/components/Navbar.jsx
@@ -38,10 +38,10 @@ const Navbar = () => {
   };
   return (
     <div
-      className="w-full md:h-12 sm:h-14 h-18 pb-4 md:pb-0 mt-2 md:mt-0 flex flex-col md:flex-row justify-between 
+      className="w-full md:h-12 h-18 pb-4 md:pb-0 flex flex-col md:flex-row justify-between 
     items-center xl:px-36 lg:px-24 md:px-12 sm:px-6 px-4 fixed top-0 z-50 bg-white dark:bg-gray-900 transition-colors duration-500"
     >
-      <div className="flex items-center sm:gap-x-4 gap-x-2">
+      <div className="flex items-center sm:gap-x-4 gap-x-2 pt-2 md:pt-0">
         <a
           href="#"
           className="md:text-2xl sm:text-xl text-lg"
