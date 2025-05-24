788ddc1478d1e49cfaaf36351ab6266d5f5b2093
kyle <kyle.harwick29@gmail.com>
2025-05-24 14:08:03 -0400

    updating about me

--- portfolio-website/src/data/index.js
+++ portfolio-website/src/data/index.js
@@ -9,7 +9,7 @@ export const letters = [
 
 import "react-tooltip/dist/react-tooltip.css";
 
-export const professionTexts = ['a Front End', 'a Back End', 'a Full Stack',]
+export const professionTexts = ["a Front End", "a Back End", "a Full Stack"];
 
 export const socialIcons = [
   { icon: "bx bxl-instagram", link: "https://www.instagram.com/khar7/" },
@@ -21,103 +21,73 @@ export const socialIcons = [
   { icon: "bx bxl-discord-alt", link: "https://discordapp.com/users/itkc" },
 ];
 
-
-export const aboutText = `Nick Brown is a dedicated full-stack web developer with a deep passion for building
-modern, high-performance web applications. With a strong foundation in both front-end
-and back-end development, he specializes in creating seamless digital experiences that
-combine functionality, efficiency, and aesthetics. His expertise spans across
-technologies such as HTML, CSS, JavaScript, React, and Node.js, allowing him to
-develop fully responsive, interactive, and scalable applications tailored to meet
-diverse client needs. From conceptualizing innovative ideas to implementing robust
-solutions, Nick takes a meticulous approach to every project he undertakes. He has a
-keen eye for UI/UX design, ensuring that every website or application he builds is not
-only visually appealing but also intuitive and user-friendly. His commitment to
-performance optimization allows him to deliver fast-loading websites with smooth
-navigation, ensuring a flawless experience across all devices and screen sizes. Nick's
-back-end skills complement his front-end expertise, enabling him to create secure and
-efficient server-side architectures using databases like MongoDB and MySQL. He is
-well-versed in API development, authentication, and data handling, which ensures that
-his applications operate seamlessly and securely. His ability to bridge the gap
-between design and functionality makes him a well-rounded developer capable of
-handling complex projects from start to finish. Beyond his technical skills, Nick is
-an enthusiastic learner who continuously explores emerging technologies and stays up
-to date with the latest industry trends. He is an active participant in the web
-development community, engaging in discussions, contributing to open-source projects,
-and sharing insights with fellow developers. His belief in continuous growth and
-innovation drives him to push boundaries and refine his skills with every project. For
-Nick, web development is more than just writing code—it's about creating meaningful
-digital experiences that leave a lasting impact. Whether he's designing a sleek
-landing page, developing a complex web application, or optimizing an existing
-platform, he approaches every challenge with dedication, creativity, and precision.
-His goal is to build solutions that not only meet client expectations but also elevate
-the standard of web development, ensuring that every project he works on stands out in
-the digital landscape.`
+export const aboutText = `Hi! I’m Kyle Harwick, a recent University of Pittsburgh undergraduate with a bachelor’s in science in Digital Narrative and Interactive Design (DNID), with a minor in Japanese Studies. DNID combines the narrative, world-building, creative elements of the English department with the coding, technical, software parts of the School of Computing and Informatics to allow for a better understanding of the intricacies of interactivity and narrative by implementing these in embedded computational systems. My interests focus on UX/UI and web development, and I have experience in frontend, backend, and overall full stack development. I have experience in HTML, CSS, JavaScript, Python, React, React Native, Next.js, and Java, and I am always looking for opportunities to learn new and exciting programming languages. In my free time I enjoy playing video games, watching and playing soccer, listening to music, reading and traveling. Please take a look at my site and feel free to reach out on LinkedIn, Discord, or even shoot me an email!`;
 
 export const skillCards = [
   {
-    icon: 'bx bx-pen',
-    title: 'Design',
+    icon: "bx bx-pen",
+    title: "Design",
     description:
-      'Design is all about creating visually appealing and user-friendly interfaces. I specialize in UI/UX design, ensuring every project balances aesthetics with functionality. Using tools like Figma and Adobe XD, I craft intuitive layouts that enhance user experience. From wireframes to high-fidelity prototypes, I focus on clean, minimal, and modern designs tailored for web and mobile applications.',
+      "Design is all about creating visually appealing and user-friendly interfaces. I specialize in UI/UX design, ensuring every project balances aesthetics with functionality. Using tools like Figma and Adobe XD, I craft intuitive layouts that enhance user experience. From wireframes to high-fidelity prototypes, I focus on clean, minimal, and modern designs tailored for web and mobile applications.",
     projectCount: 3,
     hoverPosition: {
-      large: 'bottom',
-      small: 'bottom',
+      large: "bottom",
+      small: "bottom",
     },
   },
   {
-    icon: 'bx bx-camera',
-    title: 'Photography',
+    icon: "bx bx-camera",
+    title: "Photography",
     description:
-      'Photography is an essential part of digital content creation, capturing high-quality visuals that enhance branding and storytelling. I have experience in portrait, landscape, and product photography, ensuring every image is edited and optimized for the web. Understanding lighting, composition, and post-processing allows me to create compelling images that elevate visual communication across various platforms.',
+      "Photography is an essential part of digital content creation, capturing high-quality visuals that enhance branding and storytelling. I have experience in portrait, landscape, and product photography, ensuring every image is edited and optimized for the web. Understanding lighting, composition, and post-processing allows me to create compelling images that elevate visual communication across various platforms.",
     projectCount: 4,
     hoverPosition: {
-      large: 'bottom',
-      small: 'bottom',
+      large: "bottom",
+      small: "bottom",
     },
   },
   {
-    icon: 'bx bxs-file-doc',
-    title: 'Front-End Dev',
+    icon: "bx bxs-file-doc",
+    title: "Front-End Dev",
     description:
-      'Front-end development is about bringing designs to life through clean and efficient code. I specialize in HTML, CSS, and JavaScript, along with frameworks like React to build dynamic and interactive web applications. My focus is on performance, accessibility, and responsiveness, ensuring that every website functions flawlessly across all devices and screen sizes.',
+      "Front-end development is about bringing designs to life through clean and efficient code. I specialize in HTML, CSS, and JavaScript, along with frameworks like React to build dynamic and interactive web applications. My focus is on performance, accessibility, and responsiveness, ensuring that every website functions flawlessly across all devices and screen sizes.",
     projectCount: 4,
     hoverPosition: {
-      large: 'bottom',
-      small: 'bottom',
+      large: "bottom",
+      small: "bottom",
     },
   },
   {
-    icon: 'bx bxs-devices',
-    title: 'RWD',
+    icon: "bx bxs-devices",
+    title: "RWD",
     description:
       "In today's digital world, ensuring a seamless user experience across all devices is crucial. I implement responsive design techniques using modern CSS frameworks like Tailwind CSS and Bootstrap. By employing fluid grids, flexible layouts, and media queries, I create web pages that look great and function perfectly on desktops, tablets, and smartphones.",
     projectCount: 3,
     hoverPosition: {
-      large: 'top',
-      small: 'bottom',
+      large: "top",
+      small: "bottom",
     },
   },
   {
-    icon: 'bx bx-data',
-    title: 'Back-End Dev',
+    icon: "bx bx-data",
+    title: "Back-End Dev",
     description:
-      'Back-end development powers the functionality behind web applications. I work with Node.js and databases like MongoDB and PostgreSQL to build secure and scalable server-side applications. Whether handling authentication, managing API requests, or optimizing database queries, I ensure data is processed efficiently and securely to support seamless front-end interactions.',
+      "Back-end development powers the functionality behind web applications. I work with Node.js and databases like MongoDB and PostgreSQL to build secure and scalable server-side applications. Whether handling authentication, managing API requests, or optimizing database queries, I ensure data is processed efficiently and securely to support seamless front-end interactions.",
     projectCount: 3,
     hoverPosition: {
-      large: 'top',
-      small: 'bottom',
+      large: "top",
+      small: "bottom",
     },
   },
   {
-    icon: 'bx bxl-github',
-    title: 'Git Version Control',
+    icon: "bx bxl-github",
+    title: "Git Version Control",
     description:
-      'Version control is essential for modern web development, and Git is my go-to tool for managing code efficiently. I use GitHub for collaboration, handling pull requests, merging branches, and tracking project changes. Whether working solo or in a team, I ensure code is well-documented, structured, and easily maintainable through proper versioning practices.',
+      "Version control is essential for modern web development, and Git is my go-to tool for managing code efficiently. I use GitHub for collaboration, handling pull requests, merging branches, and tracking project changes. Whether working solo or in a team, I ensure code is well-documented, structured, and easily maintainable through proper versioning practices.",
     projectCount: 3,
     hoverPosition: {
-      large: 'top',
-      small: 'bottom',
+      large: "top",
+      small: "bottom",
     },
   },
-]
+];
