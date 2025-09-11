d97b6d93391cbb4fa7745aff951172914736360e
itkc7 <kyle.harwick29@gmail.com>
2025-09-11 12:52:58 -0400

    updated loader

--- portfolio-website/src/components/Loader.jsx
+++ portfolio-website/src/components/Loader.jsx
@@ -1,13 +1,30 @@
 import React from "react";
+import { BarLoader } from "react-spinners";
 
-const Loader = ({ isLoading }) => {
+const Loader = ({
+  isLoading = true,
+  color = "#F44336",
+  size = 150,
+  cssOverride = {
+    display: "block",
+    margin: "0 auto",
+    borderColor: "red",
+  },
+}) => {
   return (
     <div
       className={`fixed inset-0 bg-white dark:bg-gray-900 z-50 grid place-items-center transition-opacity duration-1000 ease-in-out ${
         isLoading ? "opacity-100" : "opacity-0 pointer-events-none"
       }`}
     >
-      <img src="images/loading-gif-1.gif" alt="Loader" className="w-40" />
+      <BarLoader
+        color={color}
+        loading={isLoading}
+        cssOverride={cssOverride}
+        size={size}
+        aria-label="Loading Spinner"
+        data-testid="loader"
+      />
     </div>
   );
 };
